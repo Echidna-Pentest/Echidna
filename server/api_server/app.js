402a2fad567f4dab0ebaec90441b418ff038b949
shiracamus <shiracamus@gmail.com>
2024-04-09 16:58:50 +0900

    change: jude to pub for view engine

--- server/api_server/app.js
+++ server/api_server/app.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
