b0a2a702f586734ba19f047083c4c050b779af46
Kazuhisa SHIRAKAMI <k.shirakami@fujitsu.com>
2024-04-11 15:25:00 +0900

    fix: "npm run lint" now works

--- server/vue3_server/.eslintrc.js
+++ server/vue3_server/.eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
+    webextensions: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
   ],
+  parser: "vue-eslint-parser",
 }
